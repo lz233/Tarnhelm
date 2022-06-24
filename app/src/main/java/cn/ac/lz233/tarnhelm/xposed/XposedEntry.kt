a4aff870ca2932bbf8e032c5c4321f55fa51182d
QQ littleice <1767523953@qq.com>
2022-06-24 23:05:16 +0800

    xposed init

---
+++ app/src/main/java/cn/ac/lz233/tarnhelm/xposed/XposedEntry.kt
@@ -0,0 +1,12 @@
+package cn.ac.lz233.tarnhelm.xposed
+
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class XposedEntry : IXposedHookLoadPackage {
+
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+
+    }
+
+}
