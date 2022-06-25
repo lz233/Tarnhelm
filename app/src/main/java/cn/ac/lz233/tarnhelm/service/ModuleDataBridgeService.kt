55e75fff540284d25eb26041a942c323ccd09590
QQ little ice <60041789+qqlittleice233@users.noreply.github.com>
2022-06-26 01:58:39 +0800

    Xposed module working now (#3)
    * xposed init
    
    * Xposed status hook
    
    * xposed bridge between system_server and module app
    
    * Xposed module working now
    
    * roll back minSdk to 23

--- app/src/main/java/cn/ac/lz233/tarnhelm/service/ModuleDataBridgeService.kt
+++ app/src/main/java/cn/ac/lz233/tarnhelm/service/ModuleDataBridgeService.kt
@@ -3,14 +3,18 @@ package cn.ac.lz233.tarnhelm.service
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import cn.ac.lz233.tarnhelm.util.ktx.doTarnhelm
 import cn.ac.lz233.tarnhelm.xposed.ModuleDataBridge
 
 class ModuleDataBridgeService: Service() {
 
     private val binder = object : ModuleDataBridge.Stub() {
-        override fun getModuleConfig(): String {
-            return "Hello, world"
+
+        override fun doTarnhelm(s: String): String {
+            return s.doTarnhelm()
         }
+
+        override fun ping(): Int = 1
     }
 
     override fun onBind(p0: Intent): IBinder {
--- app/src/main/java/cn/ac/lz233/tarnhelm/xposed/Config.kt
+++ app/src/main/java/cn/ac/lz233/tarnhelm/xposed/Config.kt
@@ -4,5 +4,6 @@ import cn.ac.lz233.tarnhelm.BuildConfig
 
 object Config {
     const val packageName = BuildConfig.APPLICATION_ID
+    const val bridgeAction = "cn.ac.lz233.tarnhelm.bridge"
     lateinit var classLoader: ClassLoader
 }
