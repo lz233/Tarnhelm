360f8d69b8c8732fe29fe49dab3c14c29bce2a72
lz差不多是条咸鱼了 <lz23333@outlook.com>
2022-08-03 21:37:20 +0800

    add rules from web

--- app/src/main/java/cn/ac/lz233/tarnhelm/ui/process/ProcessEditTextActivity.kt
+++ app/src/main/java/cn/ac/lz233/tarnhelm/ui/process/ProcessEditTextActivity.kt
@@ -2,9 +2,11 @@ package cn.ac.lz233.tarnhelm.ui.process
 
 import android.content.Intent
 import android.os.Bundle
+import cn.ac.lz233.tarnhelm.R
 import cn.ac.lz233.tarnhelm.ui.BaseActivity
 import cn.ac.lz233.tarnhelm.util.LogUtil
 import cn.ac.lz233.tarnhelm.util.ktx.doTarnhelms
+import cn.ac.lz233.tarnhelm.util.ktx.getString
 
 class ProcessEditTextActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +15,7 @@ class ProcessEditTextActivity : BaseActivity() {
         val text = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString()
         val readOnly = intent.getBooleanExtra(Intent.EXTRA_PROCESS_TEXT_READONLY, false)
         if (readOnly) {
-            LogUtil.toast("Read Only")
+            LogUtil.toast(R.string.read_only_toast.getString())
         } else {
             setResult(RESULT_OK, Intent().apply { putExtra(Intent.EXTRA_PROCESS_TEXT, text.doTarnhelms()) })
         }
