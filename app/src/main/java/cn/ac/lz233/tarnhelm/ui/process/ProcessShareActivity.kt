701d8764d8346d6589bea597e5000733ba40a83e
lz差不多是条咸鱼了 <lz23333@outlook.com>
2022-12-27 00:19:18 +0800

    for A13, detect if sharing content is from clipboard overlay

--- app/src/main/java/cn/ac/lz233/tarnhelm/ui/process/ProcessShareActivity.kt
+++ app/src/main/java/cn/ac/lz233/tarnhelm/ui/process/ProcessShareActivity.kt
@@ -1,16 +1,42 @@
 package cn.ac.lz233.tarnhelm.ui.process
 
+import android.content.ClipData
 import android.content.ComponentName
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import cn.ac.lz233.tarnhelm.App
 import cn.ac.lz233.tarnhelm.ui.BaseActivity
+import cn.ac.lz233.tarnhelm.util.LogUtil
 import cn.ac.lz233.tarnhelm.util.ktx.doTarnhelms
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ProcessShareActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        overridePendingTransition(0, 0)
         super.onCreate(savedInstanceState)
 
+        if (Build.VERSION.SDK_INT >= 33) {
+            launch {
+                delay(50)
+                LogUtil._d(App.clipboardManager.primaryClip?.getItemAt(0)?.text)
+                LogUtil._d(intent.getStringExtra(Intent.EXTRA_TEXT))
+                if (App.clipboardManager.primaryClip?.getItemAt(0)?.text == intent.getStringExtra(Intent.EXTRA_TEXT))
+                    copyToClipboard()
+                else
+                    startChooser()
+                finish()
+            }
+        } else {
+            startChooser()
+            finish()
+        }
+
+        //finish()
+    }
+
+    private fun startChooser() {
         startActivity(Intent.createChooser(Intent().apply {
             action = Intent.ACTION_SEND
             putExtra(Intent.EXTRA_SUBJECT, (intent.getStringExtra(Intent.EXTRA_SUBJECT)?.doTarnhelms()))
@@ -20,6 +46,9 @@ class ProcessShareActivity : BaseActivity() {
         }, null).apply {
             putExtra(Intent.EXTRA_EXCLUDE_COMPONENTS, arrayOf(ComponentName(App.context, ProcessShareActivity::class.java)))
         })
-        finish()
+    }
+
+    private fun copyToClipboard() {
+        App.clipboardManager.setPrimaryClip(ClipData.newPlainText("Tarnhelm", App.clipboardManager.primaryClip?.getItemAt(0)?.text?.doTarnhelms()))
     }
 }
