4fbd93e2e1c511c6e6bfab60457d1d91ea0771ea
lz差不多是条咸鱼了 <lz23333@outlook.com>
2024-11-18 20:17:21 +0800

    Do not handle window insets manually

--- app/src/main/java/cn/ac/lz233/tarnhelm/ui/BaseActivity.kt
+++ app/src/main/java/cn/ac/lz233/tarnhelm/ui/BaseActivity.kt
@@ -4,11 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updatePadding
-import cn.ac.lz233.tarnhelm.util.LogUtil
 import com.google.android.material.appbar.MaterialToolbar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
@@ -21,12 +17,12 @@ abstract class BaseActivity : AppCompatActivity(), CoroutineScope by MainScope()
         super.onCreate(savedInstanceState)
         rootView = findViewById(android.R.id.content)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { _, insets ->
-            val systemBarInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            LogUtil.d("systemBarInsets: $systemBarInsets")
-            rootView.updatePadding(systemBarInsets.left, systemBarInsets.top, systemBarInsets.right, systemBarInsets.bottom)
-            WindowInsetsCompat.CONSUMED
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(rootView) { _, insets ->
+//            val systemBarInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            LogUtil.d("systemBarInsets: $systemBarInsets")
+//            rootView.updatePadding(systemBarInsets.left, systemBarInsets.top, systemBarInsets.right, systemBarInsets.bottom)
+//            WindowInsetsCompat.CONSUMED
+//        }
     }
 
     override fun onStart() {
